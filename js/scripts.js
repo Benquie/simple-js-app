97710f81ab894585f7cf50d6948b7fb435b6cf2a
Benon Kityo <kkbennie@gmail.com>
2019-09-16 00:48:47 +0200

    Update scripts.js
    removed extra practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
 alert('Hello world');
 var favoriteFood = 45;
 document.write(favoriteFood);
-var name = 'John';
-var age = 42;
-document.write('My name is ' + name + ' and I\'m ' + age + ' years old!');
