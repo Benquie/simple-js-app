3832a27a74225cbcaef2bc2ad30966f8fd3797e0
Benon Kityo <kkbennie@gmail.com>
2019-09-16 00:41:45 +0200

    .DS files
    DS_store files deleted

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 alert('Hello world');
 var favoriteFood = 45;
 document.write(favoriteFood);
+var name = 'John';
+var age = 42;
+document.write('My name is ' + name + ' and I\'m ' + age + ' years old!');
