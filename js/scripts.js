1d63db913be9a27558bede85007679d4fd592177
Benon Kityo <kkbennie@gmail.com>
2019-09-16 08:54:54 +0200

    updated scripts.js file
    pokemon objects added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-var favoriteFood = 45;
-document.write(favoriteFood);
+var repository = [
+{
+  name:'Butterfree',
+  height:1.1,
+  types:['bug','flying']
+},
+{
+  name:'pidgey',
+  height:0.3,
+  types:['flying','normal']
+},
+{
+  name:'metapod',
+  height:0.5,
+  types:['bug']
+}
+];
