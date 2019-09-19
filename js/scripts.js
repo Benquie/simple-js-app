eba2c97e7613004da404a4fdf47e2e3176ded5d0
Benon Kityo <kkbennie@gmail.com>
2019-09-19 17:44:26 +0200

    updated the js/script.js
    added if statement 'wow, that's big'!

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,6 @@ var repository = [
 }
 ];
 for (var i=0; i < repository.length; i++){
-    document.write( "<p>" + repository[i].name + repository[i].height + "</p>");
+    if(repository[i]["height"]>1.0)
+    document.write( "<p>" + repository[i].name + repository[i].height + "wow that's big" + "</p>");
 }
