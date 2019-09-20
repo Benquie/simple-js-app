9474e9528739b0706d85d2a58fb51222653acb26
Benon Kityo <kkbennie@gmail.com>
2019-09-20 08:34:32 +0200

    added changes
    made corrections

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,7 @@ var repository = [
 for (var i=0; i < repository.length; i++){
     if(repository[i]["height"]>1.0)
     document.write( "<p>" + repository[i].name + repository[i].height + "wow that's big" + "</p>");
+  else
+    document.write( "<p>" + repository[i].name + repository[i].height + "</p>");
+
 }
